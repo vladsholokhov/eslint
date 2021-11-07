773c7aa4c2897b81add6626b2f462cd932e6e107
Nitin Kumar <snitin315@gmail.com>
2021-11-07 03:05:06 -0800

    fix: improve fixer to cover more cases

--- tests/lib/rules/prefer-template.js
+++ tests/lib/rules/prefer-template.js
@@ -341,6 +341,16 @@ ruleTester.run("prefer-template", rule, {
             errors
         },
         {
+            code: "'a' + ('b' + 'c') + d",
+            output: "'a' + 'b' + `c${  d}`",
+            errors
+        },
+        {
+            code: "'a' + ('`b`' + '`c`') + d",
+            output: "'a' + '`b`' + `\\`c\\`${  d}`",
+            errors
+        },
+        {
             code: "var foo = \"Hello \" + \"world \" + \"another \" + test",
             output: "var foo = \"Hello \" + \"world \" + `another ${  test}`",
             errors
